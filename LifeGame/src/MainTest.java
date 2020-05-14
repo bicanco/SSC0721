fd93065164bb75b086bffa875df105a3d68af176
david <davidsourod@gmail.com>
2020-05-13 22:50:20 -0300

    Tests

---
+++ LifeGame/src/MainTest.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.junit.Test;
+
+
+public class MainTest {
+
+	@Test
+	public void testMain() {
+		assertNotNull(new Main());
+		
+		InputStream sysInput = System.in;
+		PrintStream sysOutput = System.out;
+		
+		String simulatedInput = System.lineSeparator() + "sair";
+		ByteArrayInputStream in = new ByteArrayInputStream(simulatedInput.getBytes());
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setIn(in);
+		System.setOut(new PrintStream(out));
+		
+		Main.main(null);
+		
+		try { 
+			assertEquals(3, out.toString().split(System.lineSeparator()).length);
+		} finally {		
+			System.setIn(sysInput);
+			System.setOut(sysOutput);
+		}
+
+		
+	}
+
+}
